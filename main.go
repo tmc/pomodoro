fff1de2907418bca499d23ad8e8620b13658d310
Travis Cline <travis.cline@gmail.com>
2018-10-28 14:36:34 -0700

    optionally read from environment

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 	"time"
 
 	"github.com/gen2brain/beeep"
@@ -11,10 +12,17 @@ func main() {
 	w := flag.Duration("w", 25*time.Minute, "work duration")
 	b := flag.Duration("b", 5*time.Minute, "break duration")
 	flag.Parse()
+	startMessage, breakMessage := "Begin Pomodoro session", "Time for a break"
+	if m := os.Getenv("START_MESSAGE"); m != "" {
+		startMessage = m
+	}
+	if m := os.Getenv("BREAK_MESSAGE"); m != "" {
+		breakMessage = m
+	}
 	for {
-		beeep.Alert("pomodoro", "Begin Pomodoro session", "")
+		beeep.Alert("pomodoro", startMessage, "")
 		time.Sleep(*w)
-		beeep.Alert("pomodoro", "Time for a break", "")
+		beeep.Alert("pomodoro", breakMessage, "")
 		time.Sleep(*b)
 	}
 }
